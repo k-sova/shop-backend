e5444653c8a90a700f8f838ca2424b20484feec3
Kostya Sheklein <kosheklein@ya.ru>
2026-02-24 19:52:51 +0300

    add models

---
+++ app/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine, Column, Integer, String, Float
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+
+# Строка подключения для SQLite
+DATABASE_URL = "sqlite:///ecommerce.db"
+
+# Создаём Engine
+engine = create_engine(DATABASE_URL, echo=True)
+
+# Настраиваем фабрику сеансов
+SessionLocal = sessionmaker(bind=engine)
+
+# Определяем базовый класс для моделей
+class Base(DeclarativeBase):  # New
+    pass
