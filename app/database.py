aefb8b48676971b04bc74661abe122cdc931a11f
Kostya Sheklein <kosheklein@ya.ru>
2026-02-25 17:26:50 +0300

    add async connect in db

--- app/database.py
+++ app/database.py
@@ -11,6 +11,19 @@ engine = create_engine(DATABASE_URL, echo=True)
 # Настраиваем фабрику сеансов
 SessionLocal = sessionmaker(bind=engine)
 
-# Определяем базовый класс для моделей
-class Base(DeclarativeBase):  # New
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+
+# Строка подключения для PostgreSQl
+DATABASE_URL = "postgresql+asyncpg://admin:admin@localhost:5432/db"
+
+# Создаём Engine
+async_engine = create_async_engine(DATABASE_URL, echo=True)
+
+# Настраиваем фабрику сеансов
+async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
+
+
+class Base(DeclarativeBase):
     pass
