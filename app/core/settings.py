1620efe4bcedb3140c82b6921eadd544833ce8c9
Kostya Sheklein <kosheklein@ya.ru>
2026-02-26 13:07:56 +0300

    Вынес настройки в .env

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,13 +1,19 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseModel
 
-class JWTToken(BaseSettings):
+class JWTToken(BaseModel):
     token: str
+    algorithm: str
+    
+    @property
+    def get_algorithm(self):
+        return self.algorithm
     
     @property
     def get_token(self):
         return self.token
 
-class DBSettigns(BaseSettings):    
+class DBSettigns(BaseModel):    
     postgres_user: str
     postgres_password: str
     postgres_db: str
@@ -19,10 +25,12 @@ class DBSettigns(BaseSettings):
         return f'postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}'
     
     
-class Settings:
-    model_config = SettingsConfigDict(env_file=".env")
-    def __init__(self):        
-        self.db = DBSettigns()
-        self.jwt = JWTToken()
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_nested_delimiter="__"
+    )
+    db: DBSettigns
+    jwt: JWTToken
         
 settings = Settings()
--- app/migrations/env.py
+++ app/migrations/env.py
@@ -7,12 +7,14 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-from app.database import Base
+from app.core.database import Base
 from app import models
+from app.core.settings import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.db.get_db)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
